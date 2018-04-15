be1ba8af73f80dbe96e99357f41b278763f8439f
ajaykumar <ajaykumar@192.168.43.63>
2018-04-15 17:23:36 +0530

    added tomcat auto start, iterate list

--- ThymeLeafExample/src/main/java/com/app/basic/UserController.java
+++ ThymeLeafExample/src/main/java/com/app/basic/UserController.java
@@ -1,6 +1,9 @@
 package com.app.basic;
 
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +11,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
+	
     @RequestMapping("/hello")
     public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
         model.addAttribute("name", name);
+        List< String> list=new LinkedList<>();
+        for(int i=0;i<=15;i++) {
+        		list.add("Hello"+i);
+        }
+        model.addAttribute("list",list);
         return "helloThyme"; 
-    }
-	
+    }	
 }
